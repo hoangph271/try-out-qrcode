e2d438429bfab6591d4cf69f72ca1face9c65f3a
Phan Huy Hoang <hoangph271@gmail.com>
2024-06-18 15:45:30 +0700

    add formats props

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,29 @@ function App() {
           {output.map(it => <div key={it}>{it}</div>)}
         </div>
         <Scanner
+          formats={[
+            'qr_code',
+            'micro_qr_code',
+            'rm_qr_code',
+            'maxi_code',
+            'pdf417',
+            'aztec',
+            'data_matrix',
+            'matrix_codes',
+            'dx_film_edge',
+            'databar',
+            'databar_expanded',
+            'codabar',
+            'code_39',
+            'code_93',
+            'code_128',
+            'ean_8',
+            'ean_13',
+            'itf',
+            'linear_codes',
+            'upc_a',
+            'upc_e'
+          ]}
           classNames={{
             container: "Scanner",
           }}
