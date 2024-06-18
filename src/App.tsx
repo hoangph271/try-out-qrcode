30f0703e27ea7d02dd5d37588cdf880030798c51
Phan Huy Hoang <hoangph271@gmail.com>
2024-06-18 15:35:05 +0700

    add output

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
+import { useState } from 'react';
 import { Scanner } from '@yudiel/react-qr-scanner';
 
 import './App.css'
 
 function App() {
+  const [output, setOutput] = useState<string[]>([])
 
   return (
     <>
       <div>
+        <div>
+          {output.map(it => <div key={it}>{it}</div>)}
+        </div>
         <Scanner
           classNames={{
             container: "Scanner",
           }}
           onScan={res => {
-            window.alert(res[0].rawValue);
+            setOutput(res.map(it => it.rawValue))
           }}
         />
       </div>
